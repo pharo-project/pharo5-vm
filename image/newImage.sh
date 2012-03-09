c76d0eaca12f01d515efb48ff51e9d065357813f
Camillo Bruni <camillobruni@gmail.com>
2012-03-09 14:05:23 +0100

    trying to make the build instructions clearer

--- image/newImage.sh
+++ image/newImage.sh
@@ -5,13 +5,15 @@ PREBUILT_IMAGE_URL="https://ci.lille.inria.fr/pharo/view/VM-dev/job/Cog%20Git%20
 URL="https://ci.lille.inria.fr/pharo/view/Pharo%201.4/job/Pharo%201.4/lastSuccessfulBuild/artifact/"
 VERSION="Pharo-1.4"
 
+NO_COLOR='\033[0m' #disable any colors
+YELLOW='\033[0;33m'
 # ----------------------------------------------------------------------------
 
 openImage() {
     IMAGE=$1
     SCRIPT="$PWD/ConfigurationOfCog.st"
 
-    echo 'OPENING IMAGE'
+    echo -e $YELLOW 'OPENING IMAGE' $NO_COLOR
     echo "    $IMAGE"
     echo "    $SCRIPT"
    
@@ -28,7 +30,7 @@ openImage() {
 }
 
 # ----------------------------------------------------------------------------
-echo "LOADING PREBUILT IMAGE"
+echo -e $YELLOW "LOADING PREBUILT IMAGE" $NO_COLOR
 echo "    $PREBUILT_IMAGE_URL"
 
 curl "$PREBUILT_IMAGE_URL" -o "image.zip" && \
@@ -38,14 +40,14 @@ openImage "$PWD/generator.image" && exit 1
 
 
 # ----------------------------------------------------------------------------
-echo "FETCHING FRESH IMAGE"
+echo -e $YELLOW "FETCHING FRESH IMAGE" $NO_COLOR
 echo "   $URL$VERSION.zip"
 
 # using curl since that's installed by default on mac os x :/
 curl "$URL$VERSION.zip" -o "image.zip"
 
 # ----------------------------------------------------------------------------
-echo "UNZIPPING"
+echo -e $YELLOW "UNZIPPING" $NO_COLOR
 echo "    $PWD/image.zip"
 
 unzip image.zip && \
