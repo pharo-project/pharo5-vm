581a11414063fec0e53f047ca90de5e1b6d3b188
GuillePolito <guillermopolito@gmail.com>
2013-08-28 15:11:08 +0200

    updated newImage script

--- image/newImage.sh
+++ image/newImage.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-PREBUILT_IMAGE_URL="https://ci.inria.fr/pharo/job/Cog-Git-Tracker/lastSuccessfulBuild/artifact/vmmaker-image.zip"
-URL="https://files.pharo.org/image/14/latest.zip"
-VERSION="Pharo-1.4"
-
 NO_COLOR='\033[0m' #disable any colors
 YELLOW='\033[0;33m'
 
@@ -13,65 +9,11 @@ wget --help | grep -- "$WGET_CERTCHECK" 2>&1 > /dev/null || WGET_CERTCHECK=''
 
 # ----------------------------------------------------------------------------
 
-openImage() {
-    IMAGE=$1
-    SCRIPT=$2
+echo -e "${YELLOW}LOADING STABLE 2.0 IMAGE AND VM" $NO_COLOR
 
-    echo -e "${YELLOW}OPENING IMAGE" $NO_COLOR
-    echo "    $IMAGE"
-    echo "    $SCRIPT"
-    
-    [ -z $PHAROVM ]  ||   ( echo "using " $PHAROVM \
-        &&  $PHAROVM  "$IMAGE" "$SCRIPT"; exit 0 )
-    
-    [ -z $PHARO_VM ] ||   ( echo "using " $PHARO_VM \
-        &&  $PHARO_VM  "$IMAGE" "$SCRIPT"; exit 0 )
-    
-    [ -z $SQUEAKVM ] ||   ( echo "using " $SQUEAKVM \
-        &&  $SQUEAKVM  "$IMAGE" "$SCRIPT"; exit 0 )
+echo -e "wget -O- $WGET_CERTCHECK get.pharo.org/20+vm | bash"
+wget -O- $WGET_CERTCHECK get.pharo.org/20+vm | bash
 
-    for vm in CogVM cog pharo squeak StackVM stackVM; do
-        hash $vm 2>&-    && echo "using " `which ${vm}` \
-            && $vm  "$IMAGE" "$SCRIPT"; exit 0
-    done  
-    
-    hash gnome-open 2>&- && echo "using " `which gnome-open` \
-        && gnome-open "$IMAGE"; exit 0
-    
-    hash open 2>&-       && echo "using " `which open` \
-        && open "$IMAGE" --args "$SCRIPT"; exit 0
-}
-
-# ----------------------------------------------------------------------------
-echo -e "${YELLOW}LOADING PREBUILT IMAGE" $NO_COLOR
-echo "    $PREBUILT_IMAGE_URL"
-
-wget $WGET_CERTCHECK "$PREBUILT_IMAGE_URL" --output-document="image.zip" && \
-unzip image.zip && \
-rm image.zip  && \
-openImage "$PWD/generator.image" "$PWD/ImageConfiguration.st" && exit 1
-
-
-# ----------------------------------------------------------------------------
-echo -e "${YELLOW}FETCHING FRESH IMAGE" $NO_COLOR
-echo "   $URL$VERSION.zip"
-
-wget $WGET_CERTCHECK "$URL$VERSION.zip" --output-document="image.zip"
-
-# ----------------------------------------------------------------------------
-echo -e "${YELLOW}UNZIPPING" $NO_COLOR
-echo "    $PWD/image.zip"
-
-unzip image.zip && \
-rm image.zip    && \
-mv $VERSION/*  .   && \
-rm -rf $VERSION    || exit 1
-
-
-# ----------------------------------------------------------------------------
-# try to open the image...
-set -e
-openImage "$PWD/$VERSION.image" "$PWD/../codegen-scripts/LoadVMMaker.st"
-rm -rf "$PWD/$VERSION.image" "$PWD/$VERSION.changes";
-openImage "$PWD/$generator.image" "$PWD/ImageConfiguration.st"
-
+echo -e "${YELLOW}LOADING VM MAKER SOURCES INTO IMAGE" $NO_COLOR
+echo -e "./pharo Pharo.image ../codegen-scripts/LoadVMMaker.st"
+./pharo Pharo.image ../codegen-scripts/LoadVMMaker.st
