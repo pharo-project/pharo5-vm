e8b141f25729bcf4fb4088cd8442909f285ca11c
Camillo Bruni <camillobruni@gmail.com>
2013-11-09 17:54:38 +0100

    extract apt-get update out of the ubuntu setup

--- scripts/setup-ubuntu.sh
+++ scripts/setup-ubuntu.sh
@@ -9,7 +9,6 @@ elif [ $# -gt 0 ]; then
 fi
 
 # INSTALL BUILD LIBRARIES ======================================================
-sudo apt-get update 
 sudo apt-get install cmake zip bash-completion ruby git xz-utils debhelper devscripts
 sudo apt-get install libc6-dev:i386 libasound2-dev:i386 libssl-dev:i386 libssl0.9.8:i386 libfreetype6-dev:i386 libx11-dev:i386 libsm-dev:i386 libice-dev:i386
 sudo apt-get install build-essential gcc-multilib g++
