2dcca90020e2347421b0c04b6049a7095910b9e9
Camillo Bruni <camillobruni@gmail.com>
2013-11-09 16:30:12 +0100

    added missing headless argument for VM test

--- scripts/test.sh
+++ scripts/test.sh
@@ -48,6 +48,11 @@ if [ -z "$PHARO_TEST_VM" ]; then
 fi
 
 # RUN TEST IMAGE ==============================================================
-
+if [ "$OS" == "linux" ]; then
+	HEADLESS="-nodisplay"
+else
+	HEADLESS="-headless"
+fi
+	
 TEST_IMAGE=`"$SCRIPT_DIR/../image/newVMTestImage.sh"`
-"$PHARO_TEST_VM" "$TEST_IMAGE" test ".*"
+"$PHARO_TEST_VM" $HEADLESS "$TEST_IMAGE" test ".*"
