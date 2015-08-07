b4f66b8d7b53e052e73c02f49c3fdd5c89e852e1
Max Leske <maxleske@gmail.com>
2015-08-07 17:16:07 +0200

    when you define a variable you should actually use it...

--- scripts/windows/start-build.sh
+++ scripts/windows/start-build.sh
@@ -61,7 +61,7 @@ function fixHeadersAndLibs() {
 
   # copy fixed float.h
   float_path=$(find "${mingw_dir}/lib/gcc/" -regex ".*/include/float.h")
-  cp "platforms/win32/extras/float.h" ${float_path%*/}
+  cp "${pharovm-dir}/platforms/win32/extras/float.h" ${float_path%*/}
 
   # add missing defs to _mingw.h
   echo "#define off64_t _off64_t" >> "${mingw_dir}/include/_mingw.h"
@@ -72,7 +72,7 @@ function fixHeadersAndLibs() {
   # echo "#define off_t _off_t" >> "${mingw_dir}/mingw32/include/_mingw.h"
 
   # copy libcrtdll (obsolete but needed)
-  cp "platforms/win32/extras/libcrtdll.a" "${mingw_dir}/lib"
+  cp "${pharovm_dir}/platforms/win32/extras/libcrtdll.a" "${mingw_dir}/lib"
 }
 
 
