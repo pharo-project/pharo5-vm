c9d746de0b1d7acc9036c923cf9b293c464994ac
Esteban Lorenzano <estebanlm@gmail.com>
2016-03-09 14:20:01 +0100

    another simplification

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,29 +1,16 @@
 #!/bin/bash 
 set -ex
 
-echo "DIR: `pwd`"
-
-# VM PROPERTIES ===============================================================
-VM_TYPE="pharo"
-VM_BINARY_NAME="Pharo"
-VM_BINARY_NAME_LINUX="pharo"
-VM_DIR="results"
-
-
 # TEST VM LOCATION ============================================================
-TMP_OS=`uname | tr "[:upper:]" "[:lower:]"`
-if [[ "{$TMP_OS}" = *darwin* ]]; then
-    PHARO_TEST_VM=`find "$VM_DIR" -name ${VM_BINARY_NAME}`;
-elif [[ "{$TMP_OS}" = *linux* ]]; then
-    PHARO_TEST_VM=`find "$VM_DIR" -name ${VM_BINARY_NAME_LINUX}`;
-elif [[ "{$TMP_OS}" = *win* ]]; then
-    PHARO_TEST_VM=`find "$VM_DIR" -name ${VM_BINARY_NAME}.exe`;
-elif [[ "{$TMP_OS}" = *mingw* ]]; then
-    PHARO_TEST_VM=`find "$VM_DIR" -name ${VM_BINARY_NAME}.exe`;
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+	VM_NAME="pharo"
+elif [ "$TRAVIS_OS_NAME" = "osx" ]]; then
+	VM_NAME="Pharo"
 else
     echo "Unsupported OS";
     exit 1;
 fi
+PHARO_TEST_VM=`find results -name $VM_NAME`;
 
 if [ -z "$PHARO_TEST_VM" ]; then
 	echo "Could not find test VM in $VM_DIR";
@@ -31,17 +18,14 @@ if [ -z "$PHARO_TEST_VM" ]; then
 	exit 1
 fi
 
-
 # ENSURE SOURCES FILE =========================================================
 cp image/pharo-vm/*.sources $VM_DIR
 
 
 # RUN TEST IMAGE ==============================================================
-if [ "$OS" == "linux" ]; then
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 	HEADLESS="--nodisplay"
 else
 	HEADLESS="--headless"
 fi
-
-TEST_IMAGE="image/Pharo.image"
-"$PHARO_TEST_VM" $HEADLESS "$TEST_IMAGE" test --no-xterm ".*"
+"$PHARO_TEST_VM" $HEADLESS image/Pharo.image test --no-xterm ".*"
