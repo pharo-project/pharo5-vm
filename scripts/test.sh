ca1aea8737356be8e776c9c5c68121cb311dbcf3
Esteban Lorenzano <estebanlm@gmail.com>
2016-03-11 15:31:51 +0100

    test pwd

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,8 +11,9 @@ else
     echo "Unsupported OS";
     exit 1;
 fi
-PHARO_TEST_VM=`find $VM_DIR -name $VM_NAME`;
 
+echo "DIR: `pwd`"
+PHARO_TEST_VM=`find $VM_DIR -name $VM_NAME`;
 if [ -z "$PHARO_TEST_VM" ]; then
 	echo "Could not find test VM in $VM_DIR";
 	echo "Build the VM with scripts/build.sh"
@@ -34,6 +35,7 @@ NO_TEST="$NO_TEST(?!Versionner)"	# too long
 NO_TEST="$NO_TEST(?!GT)"			# too slow
 NO_TEST="$NO_TEST(?!Athens)"		# no cairo, no athens
 NO_TEST="$NO_TEST(?!OSWindow)"		# no cairo, no oswindow
+
 NO_TEST="$NO_TEST(?!TxText)"		# no cairo, no TxText
 NO_TEST="$NO_TEST(?!ReleaseTests)"	# just not now :)
 "$PHARO_TEST_VM" $HEADLESS image/Pharo.image test --no-xterm --fail-on-failure "$NO_TEST[A-Z].*"
