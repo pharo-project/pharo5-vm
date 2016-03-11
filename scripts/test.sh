77764601d3e5814727346a213c1075bf051e2bf5
Esteban Lorenzano <estebanlm@gmail.com>
2016-03-11 15:57:03 +0100

    remove echo

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,7 +12,6 @@ else
     exit 1;
 fi
 
-echo "DIR: `pwd`"
 PHARO_TEST_VM=`find $VM_DIR -name $VM_NAME`;
 if [ -z "$PHARO_TEST_VM" ]; then
 	echo "Could not find test VM in $VM_DIR";
@@ -35,7 +34,6 @@ NO_TEST="$NO_TEST(?!Versionner)"	# too long
 NO_TEST="$NO_TEST(?!GT)"			# too slow
 NO_TEST="$NO_TEST(?!Athens)"		# no cairo, no athens
 NO_TEST="$NO_TEST(?!OSWindow)"		# no cairo, no oswindow
-
 NO_TEST="$NO_TEST(?!TxText)"		# no cairo, no TxText
 NO_TEST="$NO_TEST(?!ReleaseTests)"	# just not now :)
 "$PHARO_TEST_VM" $HEADLESS image/Pharo.image test --no-xterm --fail-on-failure "$NO_TEST[A-Z].*"
