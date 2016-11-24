cd9bed8b378433f03fb891fc709f115008fb0bee
Esteban Lorenzano <estebanlm@gmail.com>
2016-11-24 13:01:46 +0100

    remove cmd execution

--- scripts/deploy-appveyor.sh
+++ scripts/deploy-appveyor.sh
@@ -1,9 +1,6 @@
 # Deploy in appveyor environment
-
 appveyor DownloadFile https://curl.haxx.se/ca/cacert.pem
 export SSL_CERT_FILE=cacert.pem
-export PATH="C:\\Ruby23\\bin:$PATH"
-export CMDSHELL="cmd /C "
 
-$CMDSHELL gem install dpl
-$CMDSHELL dpl --provider=bintray --user=$BINTRAY_USER --key=$BINTRAY_API_KEY --file=.bintray.json
+gem install dpl
+dpl --provider=bintray --user=$BINTRAY_USER --key=$BINTRAY_API_KEY --file=.bintray.json
