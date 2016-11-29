aa6950ec14993f0ecc294f1ba24c42afe449b8eb
Esteban Lorenzano <estebanlm@gmail.com>
2016-11-29 17:16:00 +0100

    deploy on master branch

--- scripts/deploy-appveyor.sh
+++ scripts/deploy-appveyor.sh
@@ -1,6 +1,11 @@
 # Deploy in appveyor environment
+
+# deploy on master branch
+if [ "$APPVEYOR_REPO_BRANCH" != "master" ]; then
+	exit 
+fi
+
 appveyor DownloadFile https://curl.haxx.se/ca/cacert.pem
 export SSL_CERT_FILE=cacert.pem
-
 gem install dpl
 dpl --provider=bintray --user=$BINTRAY_USER --key=$BINTRAY_API_KEY --file=.bintray.json
