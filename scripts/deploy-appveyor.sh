f58bd7d51c45bcacc511d3978a817276f838f3f4
Esteban Lorenzano <estebanlm@gmail.com>
2016-12-09 08:55:01 +0100

    inhance a bit appveyor-deploy script so see what happens

--- scripts/deploy-appveyor.sh
+++ scripts/deploy-appveyor.sh
@@ -2,14 +2,17 @@
 
 # deploy if repository is pharo-project/pharo-vm
 if [ "$APPVEYOR_REPO_NAME" != "pharo-project/pharo-vm" ]; then
+	echo "Trying to deploy in repository: $APPVEYOR_REPO_NAME. Skipping."
 	exit 
 fi
 
 # deploy on master branch
 if [ "$APPVEYOR_REPO_BRANCH" != "master" ]; then
+	echo "Trying to deploy in branch: $APPVEYOR_REPO_BRANCH. Skipping."
 	exit 
 fi
 
+echo "Deploying to bintray with user $BINTRAY_USER"
 appveyor DownloadFile https://curl.haxx.se/ca/cacert.pem
 export SSL_CERT_FILE=cacert.pem
 gem install dpl
