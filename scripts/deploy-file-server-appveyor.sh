02381a48a2455821d56c669c04d63120d1b93301
Esteban Lorenzano <estebanlm@gmail.com>
2017-01-10 16:43:08 +0100

    add appveyor deploy

---
+++ scripts/deploy-file-server-appveyor.sh
@@ -0,0 +1,13 @@
+# deploy if repository is pharo-project/pharo-vm
+#if [ "$APPVEYOR_REPO_NAME" != "pharo-project/pharo-vm" ]; then
+#	echo "Trying to deploy in repository: $APPVEYOR_REPO_NAME. Skipping."
+#	exit 
+#fi
+
+# deploy on master branch
+if [ "$APPVEYOR_REPO_BRANCH" != "master" ]; then
+	echo "Trying to deploy in branch: $APPVEYOR_REPO_BRANCH. Skipping."
+	exit 
+fi
+
+sh `dirname $0`/deploy-file-server.sh
