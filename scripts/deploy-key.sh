6a1b12816db1807e304e5a21d66a8ce82be6c032
Esteban Lorenzano <estebanlm@gmail.com>
2017-01-10 12:07:53 +0100

    add config to deploy-key.sh

--- scripts/deploy-key.sh
+++ scripts/deploy-key.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 
+set -e
+
 openssl aes-256-cbc -K $DEPLOY_KEY -iv $DEPLOY_KEY_IV -in deploy_key.enc -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
+
+echo "Host files.pharo.org
+	User $DEPLOY_USER
+	ProxyCommand ssh $DEPLOY_USER@sesi-ssh.inria.fr \"nc file-pharo.inria.fr %p 2> /dev/null\"" >> ~/.ssh/config
+
+cat ~/.ssh/config
