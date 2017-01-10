c61be8730e5a9e089221a424e1e32c279a34e9f7
Esteban Lorenzano <estebanlm@gmail.com>
2017-01-10 15:44:33 +0100

    deploying into appropriate subdirectory

--- scripts/deploy-file-server.sh
+++ scripts/deploy-file-server.sh
@@ -1,3 +1,35 @@
 #! /bin/bash
 
-scp *.zip files.pharo.org:~
+set -e 
+
+baseDir="/appli/files.pharo.org/vm"
+case "${ARCH}" in
+	macos32x86) 
+		destDir="$baseDir/pharo-spur32/mac" 
+		;;
+	macos64x64) 
+		destDir="$baseDir/pharo-spur64/mac" 
+		;;
+	linux32x86) 
+		destDir="$baseDir/pharo-spur32/linux" 
+		;;
+	linux64x64) 
+		destDir="$baseDir/pharo-spur64/linux" 
+		;;
+	linux32ARMv6) 
+		destDir="$baseDir/pharo-spur32/linux/armv6"
+		;;
+	win32x86) 
+		destDir="$baseDir/pharo-spur32/win"
+		;;
+	win64x64) 
+		destDir="$baseDir/pharo-spur64/win"
+		;;
+	*) 
+		echo "Undefined platform!"
+		exit 1
+esac
+
+productName=`find . -name "pharo-*.zip"`
+scp $productName files.pharo.org:$destDir/$productName
+scp $productName files.pharo.org:$destDir/latest.zip
--- scripts/deploy-key.sh
+++ scripts/deploy-key.sh
@@ -13,10 +13,3 @@ echo "Host files.pharo.org
 echo "files.pharo.org ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCeVezoVox1IBcyqhPZbWaHoJR1R8iYZwZuqYDCjmmp1+bXxdPwI4FNbulc8CvGS+pvmJbWe6ypIksT1BNHOySZ574+mNq73YvgXuyl8FO45D7y22l7tjdQPR3b3XZ6gcTFoW+MTSMmipujN8D71vcbyhT7xQlgnP681LGRhTKbPZBOL84w2iPWc71l7keen/LMW4ifdJR46MdrOyXFNN2pxF3o42wItAv5Koae3ydD6UBw58xQlDsJpr1XVVpxVAEW1vonMs0IYJWKBRxvJVh1k/Nx8GwqlUHxQvjiHFlCheQ6ebuARwCH7ccJETKPxbkkVH1TESzOxjZH5W/miFGD
 sesi-ssh.inria.fr ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAnOBtRNSwF5Gj4XmaPaWgIJY3GmONfC1g7/0DU9if5IpJ9Wq76t7fUmnTFBKHbmQsGWMdauPzpcu6iovB08bqZWaXNQj1xjuhj0vrRzgwV/8uYnpDMaB6d65Ef9KY8nnvQDFfD5wT3vdDht8LN4MHNQfeUzax1M0IT+w9uElotd9RgtJE8SnoFN9CGjkpP8C97V3xuXCHKYxVkWpmXIlFrbT1D4ct47DH6t3V8tZbpFbsQwWxUUWdbBsLLcYsXx+aulKiQg+5jXuFfSnA3FfLH8eIKShA0QGfGPB1XkMqffaUY8yzby9DDpBPFBj1zoXQd9qt2Zsti/5VJ38+53PTLw==
 " >> ~/.ssh/known_hosts
-
-#ssh-keyscan -H 193.51.193.142 >> ~/.ssh/known_hosts
-#ssh-keyscan -H sesi-ssh.inria.fr,193.51.193.142 >> ~/.ssh/known_hosts
-#ssh-keyscan -H sesi-ssh.inria.fr >> ~/.ssh/known_hosts
-#ssh-keyscan -H 128.93.162.72 >> ~/.ssh/known_hosts
-#ssh-keyscan -H files.pharo.org,128.93.162.72 >> ~/.ssh/known_hosts
-#ssh-keyscan -H files.pharo.org >> ~/.ssh/known_hosts
