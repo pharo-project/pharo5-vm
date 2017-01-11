1eef490aa3ca6f856f6144d3a6599d97881754a0
Esteban Lorenzano <estebanlm@gmail.com>
2017-01-11 11:47:39 +0100

    using mkdir -p to ensure the full path is created (in appveyors seems necesary)

--- scripts/deploy-key.sh
+++ scripts/deploy-key.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ ! -e ~/.shh ]; then
-	mkdir ~/.ssh
+	mkdir -p ~/.ssh
 fi
 openssl aes-256-cbc -K $DEPLOY_KEY -iv $DEPLOY_KEY_IV -in deploy_key.enc -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
