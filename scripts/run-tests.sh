ad4b8a14309090de8096effb035191581a3d3acf
Esteban Lorenzano <estebanlm@gmail.com>
2017-03-10 15:41:16 +0100

    try to run threaded vm

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -12,10 +12,12 @@ if [ "$ARCH" = "linux32ARMv6" ]; then
 	exit
 fi
 
-#if [ "$HEARTBEAT" = "threaded" ]; then
+if [ "$HEARTBEAT" = "threaded" ]; then
 #	echo "Skipping tests for threaded (It requires a special linux configuration)."
 #	exit
-#fi
+	ulimit -r 2
+	EXEC_PREFIX="exec setuidgid $TRAVIS_USER"
+fi
 
 # UNZIP packed VM
 VM_ARCHIVE=`ls pharo-*.zip`
@@ -63,4 +65,4 @@ NO_TEST="$NO_TEST(?!Athens)"		# no cairo, no athens
 NO_TEST="$NO_TEST(?!OSWindow)"		# no cairo, no oswindow
 NO_TEST="$NO_TEST(?!TxText)"		# no cairo, no TxText
 NO_TEST="$NO_TEST(?!ReleaseTests)"	# just not now :)
-"$PHARO_TEST_VM" $HEADLESS $TEST_IMAGE_DIR/Pharo.image test --no-xterm --fail-on-failure "$NO_TEST[A-Z].*"
+"$EXEC_PREFIX $PHARO_TEST_VM" $HEADLESS $TEST_IMAGE_DIR/Pharo.image test --no-xterm --fail-on-failure "$NO_TEST[A-Z].*"
