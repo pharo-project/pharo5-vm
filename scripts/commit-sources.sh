3266f4d473ff8bb9f1260678b7b0e50c57086eec
Esteban Lorenzano <estebanlm@gmail.com>
2017-03-28 11:24:49 +0200

    fix src paths

--- scripts/commit-sources.sh
+++ scripts/commit-sources.sh
@@ -14,8 +14,8 @@ git config user.email ""
 
 LAST_MESSAGE=`git log -1 --pretty=%B`
 
-git add opensmalltalk-vm/src/*
-git add opensmalltalk-vm/spursrc/*
-git add opensmalltalk-vm/spur64src/*
+git add ../opensmalltalk-vm/src/*
+git add ../opensmalltalk-vm/spursrc/*
+git add ../opensmalltalk-vm/spur64src/*
 
 git commit --amend -m "$LAST_MESSAGE"
