9b28d4a8cf16f22c8649c75704bd2c02adf6691e
Esteban Lorenzano <estebanlm@gmail.com>
2017-03-28 11:49:54 +0200

    setting user.name and user.email to commit (is required)

--- scripts/commit-sources.sh
+++ scripts/commit-sources.sh
@@ -9,13 +9,15 @@ fi
 # prepare keys (I will use same as deploy, so I just install them now)
 ./deploy-key.sh
 # set system properties
-git config user.name "Travis CI"
-git config user.email ""
+git config user.name "$GIT_USERNAME"
+git config user.email "$GIT_USERMAIL"
 
-LAST_MESSAGE=`git log -1 --pretty=%B`
+#LAST_MESSAGE=`git log -1 --pretty=%B`
 
 git add ../opensmalltalk-vm/src/*
 git add ../opensmalltalk-vm/spursrc/*
 git add ../opensmalltalk-vm/spur64src/*
 
-git commit --amend -m "$LAST_MESSAGE"
+git commit --amend --no-edit
+
+#-m "$LAST_MESSAGE"
