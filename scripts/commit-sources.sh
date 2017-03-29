7cc649f8806b4e30d337d76c234b0dfe73e42269
Esteban Lorenzano <estebanlm@gmail.com>
2017-03-29 16:18:59 +0200

    trying pushing the commit

--- scripts/commit-sources.sh
+++ scripts/commit-sources.sh
@@ -15,21 +15,20 @@ if [  "$TRAVIS_REPO_SLUG" != "estebanlm/pharo-vm" -o "$TRAVIS_BRANCH" != "master
 	exit
 fi
 
-if [ "$TRAVIS_COMMIT" != "`git rev-parse HEAD`" ]; then
-	echo "Not in HEAD, aborting."
-	exit 
-fi
+#if [ "$TRAVIS_COMMIT" != "`git rev-parse HEAD`" ]; then
+#	echo "Not in HEAD, aborting."
+#	exit 
+#fi
 
 # prepare keys (I will use same as deploy, so I just install them now)
 ./deploy-key.sh
 # set system properties
 git config user.name "\"$GIT_USERNAME\""
 git config user.email "\"$GIT_USERMAIL\""
-
-git checkout HEAD
-
+# stage changes
 git add ../opensmalltalk-vm/src/*
 git add ../opensmalltalk-vm/spursrc/*
 git add ../opensmalltalk-vm/spur64src/*
-
+# commit & push
 git commit --amend --no-edit
+git push
